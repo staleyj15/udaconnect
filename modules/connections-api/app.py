9a66f8511a6d77b4aea02c3e08d11fb28988ef5b
Jessie Staley <jessiestaley15@gmail.com>
2022-05-23 16:49:35 -0400

    reorganized structure of connections-api module

--- modules/connections-api/app.py
+++ modules/connections-api/app.py
@@ -1,23 +1,13 @@
 import os
+from src.config import config_by_name
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import Session
-from connection_client import for_testing_only
 
-DB_USERNAME = os.environ["DB_USERNAME"]
-DB_PASSWORD = os.environ["DB_PASSWORD"]
-DB_HOST = os.environ["DB_HOST"]
-DB_PORT = os.environ["DB_PORT"]
-DB_NAME = os.environ["DB_NAME"]
-
-engine = create_engine(
-    f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+env = os.getenv("FLASK_ENV") or "test"
+engine = create_engine(config_by_name[env].SQLALCHEMY_DATABASE_URI)
 session = Session(engine)
 metadata = MetaData(engine)
 
 if __name__ == "__main__":
     from src.server import start_grpc_server
     start_grpc_server()
-
-    # delete after testing
-    # for_testing_only()
